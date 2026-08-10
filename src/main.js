e8db4e66e93321c52dbb48ea62f7f0edc6d9d526
Carlos <pokejagger@gmail.com>
2026-08-10 00:35:12 -0600

    Render completo con esqueleto de animaciones

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import * as THREE from "three";
 import "./style.scss";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { OrbitControls } from "./utils/OrbitControls.js";
 import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import gsap from "gsap";
 
 const canvas = document.querySelector("#experience-canvas");
 const sizes = {
@@ -10,11 +11,77 @@ const sizes = {
   height: window.innerHeight,
 };
 
+const modals = {
+  work: document.querySelector(".modal.work"),
+  about: document.querySelector(".modal.about"),
+  contact: document.querySelector(".modal.contact"),
+  skills: document.querySelector(".modal.skills"),
+};
+
+let touchHappend = false;
+document.querySelectorAll(".modal-exit-buton").forEach((button) => {
+  button.addEventListener(
+    "touchend",
+    (e) => {
+      touchHappend = true;
+      e.preventDefault();
+      const modal = e.target.closest(".modal");
+      hideModal(modal);
+    },
+    { passive: false },
+  );
+  button.addEventListener(
+    "click",
+    (e) => {
+      if (touchHappend) return;
+      e.preventDefault();
+      const modal = e.target.closest(".modal");
+      hideModal(modal);
+    },
+    { passive: false },
+  );
+});
+
+let isModalOpen = false;
+
+const showModal = (modal) => {
+  modal.style.display = "block";
+  isModalOpen = true;
+  controls.enabled = false;
+
+  if (currentHoveredObject) {
+    playHoverAnimation(currentHoveredObject, false);
+    currentHoveredObject = null;
+  }
+  document.body.style.cursor = "default";
+  currentIntersects = [];
+
+  gsap.set(modal, { opacity: 0 });
+
+  gsap.to(modal, {
+    opacity: 1,
+    duration: 0.5,
+  });
+};
+
+const hideModal = (modal) => {
+  isModalOpen = false;
+  controls.enabled = true;
+  gsap.to(modal, {
+    opacity: 0,
+    duration: 0.5,
+    onComplete: () => {
+      modal.style.display = "none";
+    },
+  });
+};
+
 const xAxisFans = [];
 const yAxisFans = [];
 
 const raycarterObjects = [];
 let currentIntersects = [];
+let currentHoveredObject = null;
 
 const socialLinks = {
   Placadegithub: "https://github.com/CarlosAdrianLabra",
@@ -159,11 +226,33 @@ const encajarEnUVs = (mesh, texture, ajustes = {}) => {
 };
 
 window.addEventListener("mousemove", (e) => {
+  touchHappend = false;
   pointer.x = (e.clientX / window.innerWidth) * 2 - 1;
   pointer.y = -(e.clientY / window.innerHeight) * 2 + 1;
 });
 
-window.addEventListener("click", (e) => {
+window.addEventListener(
+  "touchstart",
+  (e) => {
+    if (isModalOpen) return;
+    e.preventDefault();
+    pointer.x = (e.touches[0].clientX / window.innerWidth) * 2 - 1;
+    pointer.y = -(e.touches[0].clientY / window.innerHeight) * 2 + 1;
+  },
+  { passive: false },
+);
+
+window.addEventListener(
+  "touchend",
+  (e) => {
+    if (isModalOpen) return;
+    e.preventDefault();
+    handleRaycasterInteraction();
+  },
+  { passive: false },
+);
+
+function handleRaycasterInteraction() {
   if (currentIntersects.length > 0) {
     const object = currentIntersects[0].object;
 
@@ -176,8 +265,49 @@ window.addEventListener("click", (e) => {
         newWindow.rel = "noopener noreferrer";
       }
     });
+    if (object.name.includes("Botonwork")) {
+      showModal(modals.work);
+    } else if (object.name.includes("BotonAbout")) {
+      showModal(modals.about);
+    } else if (object.name.includes("Botoncontact")) {
+      showModal(modals.contact);
+    } else if (object.name.includes("Botonskills")) {
+      showModal(modals.skills);
+    }
   }
-});
+}
+
+window.addEventListener("click", handleRaycasterInteraction);
+
+let btnAbout,
+  btnContact,
+  btnSkills,
+  btnWork,
+  fotoBoxFamilia,
+  fotoBoxPareja,
+  fotoBoxHombre,
+  gata1,
+  gata2,
+  letraJ,
+  letraA,
+  letraG,
+  letraG2,
+  letraE,
+  letraR,
+  letraL,
+  letraO,
+  letraT,
+  marcoFamilia,
+  marcoPareja,
+  marcoHombre,
+  pengu,
+  placaGithub,
+  placaLinkin,
+  placaTft,
+  placaYoutube,
+  poro,
+  mikasa,
+  taza;
 
 loader.load("/models/CuartoPortafolio26-v1.glb", (glb) => {
   glb.scene.traverse((child) => {
@@ -193,6 +323,18 @@ loader.load("/models/CuartoPortafolio26-v1.glb", (glb) => {
         child.name.includes("Mikasa")
       ) {
         raycarterObjects.push(child);
+      }
+      if (child.name.includes("Boton") || child.name.includes("Placa")) {
+        child.userData.initialScale = new THREE.Vector3().copy(child.scale);
+        child.userData.initialPosition = new THREE.Vector3().copy(
+          child.position,
+        );
+        child.userData.initialRotation = new THREE.Euler().copy(child.rotation);
+      }
+
+      if (child.name.includes("Botoncontact")) {
+        btnContact = child;
+        child.scale.set(0, 0, 0);
       }
       if (child.name.includes("Vidrio")) {
         child.material = glassMaterial;
@@ -266,7 +408,22 @@ loader.load("/models/CuartoPortafolio26-v1.glb", (glb) => {
     }
   });
   scene.add(glb.scene);
+  playIntroAnimation();
 });
+
+function playIntroAnimation() {
+  const t1 = gsap.timeline({
+    defaults: {
+      duration: 0.8,
+      ease: "back.out(1.8)",
+    },
+  });
+  t1.to(btnContact.scale, {
+    z: 1.38,
+    x: 1.38,
+    y: 1.38,
+  });
+}
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -282,6 +439,13 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.minDistance = 5;
+controls.maxDistance = 35;
+controls.minPolarAngle = 0;
+controls.maxPolarAngle = Math.PI / 2;
+controls.minAzimuthAngle = 0;
+controls.maxAzimuthAngle = Math.PI / 2;
+
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 controls.update();
@@ -301,6 +465,46 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
+function playHoverAnimation(object, isHovering) {
+  gsap.killTweensOf(object.scale);
+  gsap.killTweensOf(object.rotation);
+  gsap.killTweensOf(object.position);
+
+  const conGiro = object.name.includes("Placa");
+
+  if (isHovering) {
+    gsap.to(object.scale, {
+      x: object.userData.initialScale.x * 1.2,
+      y: object.userData.initialScale.y * 1.2,
+      z: object.userData.initialScale.z * 1.2,
+      duration: 0.5,
+      ease: "bounce.out(1.8)",
+    });
+    if (conGiro) {
+      gsap.to(object.rotation, {
+        x: object.userData.initialRotation.x + Math.PI / 8,
+        duration: 0.5,
+        ease: "bounce.out(1.8)",
+      });
+    }
+  } else {
+    gsap.to(object.scale, {
+      x: object.userData.initialScale.x,
+      y: object.userData.initialScale.y,
+      z: object.userData.initialScale.z,
+      duration: 0.3,
+      ease: "bounce.out(1.8)",
+    });
+    if (conGiro) {
+      gsap.to(object.rotation, {
+        x: object.userData.initialRotation.x + Math.PI / 8,
+        duration: 0.3,
+        ease: "bounce.out(1.8)",
+      });
+    }
+  }
+}
+
 const render = () => {
   controls.update();
   //console.log(camera.position);
@@ -317,25 +521,44 @@ const render = () => {
   });
 
   //Raycaster
-  raycaster.setFromCamera(pointer, camera);
+  if (!isModalOpen) {
+    raycaster.setFromCamera(pointer, camera);
 
-  // calculate objects intersecting the picking ray
-  currentIntersects = raycaster.intersectObjects(raycarterObjects);
+    // calculate objects intersecting the picking ray
+    currentIntersects = raycaster.intersectObjects(raycarterObjects);
 
-  for (let i = 0; i < currentIntersects.length; i++) {}
+    for (let i = 0; i < currentIntersects.length; i++) {}
 
-  if (currentIntersects.length > 0) {
-    const currentInterserctObject = currentIntersects[0].object;
-    if (
-      currentInterserctObject.name.includes("Boton") ||
-      currentInterserctObject.name.includes("Placa")
-    ) {
-      document.body.style.cursor = "pointer";
+    if (currentIntersects.length > 0) {
+      const currentInterserctObject = currentIntersects[0].object;
+
+      if (
+        currentInterserctObject.name.includes("Boton") ||
+        currentInterserctObject.name.includes("Placa")
+      ) {
+        if (currentInterserctObject !== currentHoveredObject) {
+          if (currentHoveredObject) {
+            playHoverAnimation(currentHoveredObject, false);
+          }
+          playHoverAnimation(currentInterserctObject, true);
+          currentHoveredObject = currentInterserctObject;
+        }
+      }
+      if (
+        currentInterserctObject.name.includes("Boton") ||
+        currentInterserctObject.name.includes("Placa")
+      ) {
+        document.body.style.cursor = "pointer";
+      } else {
+        document.body.style.cursor = "default";
+      }
     } else {
+      if (currentHoveredObject) {
+        playHoverAnimation(currentHoveredObject, false);
+        currentHoveredObject = null;
+      }
       document.body.style.cursor = "default";
     }
-  } else {
-    document.body.style.cursor = "default";
   }
   renderer.render(scene, camera);
   window.requestAnimationFrame(render);
